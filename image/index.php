05847b9709acb1af06b324c7d10fd2e9fe4ebd45
RedCat <105387890+s-ssss@users.noreply.github.com>
2023-04-19 04:12:51 +0800

    Update index.php

--- image/index.php
+++ image/index.php
@@ -1,5 +1,5 @@
 <?php
-$rand = rand(1,23);//20是图片总数量
+$rand = rand(1,23);//10是图片总数量
 $file_url = 'http://s-ssss.github.io/image/' . $rand . '.jpg';
 header('Content-type:image/jpeg');
 echo file_get_contents($file_url);
